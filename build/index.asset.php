bcb035c70c0c9c7d58144d8e52050fff9948a173
Lukas Niebler <lukas.niebler@fau.de>
2023-01-30 15:49:06 +0100

    Test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b9e47936ff01ce5da8ec');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1547e9605e147af911e0');
